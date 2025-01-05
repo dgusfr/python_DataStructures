ac3ac414d2cf846372270d1131e0d5e49a03c357
dgusfr <diegogustavo.fr@gmail.com>
2025-01-05 08:32:09 -0300

    adicionado processamento das opções do menu

--- tuples.py
+++ tuples.py
@@ -24,3 +24,24 @@ def main():
             processar_opcao(opcao, tupla)
         except ValueError as e:
             print(f"Erro: {e}")
+
+def processar_opcao(opcao, tupla):
+    if opcao == '1':
+        tupla = criar_tupla()
+    elif opcao == '2':
+        tupla = concatenar_tuplas(tupla)
+    elif opcao == '3':
+        obter_elemento_por_indice(tupla)
+    elif opcao == '4':
+        contar_ocorrencias(tupla)
+    elif opcao == '5':
+        obter_indice(tupla)
+    elif opcao == '6':
+        verificar_elemento(tupla)
+    elif opcao == '7':
+        mostrar_tupla(tupla)
+    elif opcao == '8':
+        print("Saindo...")
+        sys.exit(0)
+    else:
+        print("Opção inválida. Tente novamente.")
