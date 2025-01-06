d61a4b2d1f2305702c934fb7f29ef051c1827f2a
dgusfr <diegogustavo.fr@gmail.com>
2025-01-06 09:07:03 -0300

    feat: função para atualizar valor de uma chave no dicionário

--- dictionary.py
+++ dictionary.py
@@ -21,3 +21,12 @@ def adicionar_chave_valor(dicionario):
     valor = input("Digite o valor: ")
     dicionario[chave] = valor
     print(f"Chave-valor '{chave}: {valor}' adicionada com sucesso!")
+
+def atualizar_valor(dicionario):
+    chave = input("Digite a chave para atualizar: ")
+    if chave in dicionario:
+        novo_valor = input("Digite o novo valor: ")
+        dicionario[chave] = novo_valor
+        print(f"Valor da chave '{chave}' atualizado para '{novo_valor}'.")
+    else:
+        print(f"Chave '{chave}' não encontrada.")
