6949d92ef1998b982692d817e4ce40c4f34282ad
dgusfr <diegogustavo.fr@gmail.com>
2025-01-07 09:38:19 -0300

    feat: implementar menu de opções para o conjunto

--- sets.py
+++ sets.py
@@ -2,3 +2,12 @@ import os
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def display_menu():
+    print("\nEscolha uma operação para realizar no conjunto:")
+    print("1. Adicionar um elemento")
+    print("2. Remover um elemento")
+    print("3. Verificar se um elemento existe")
+    print("4. Listar todos os elementos")
+    print("5. Limpar o conjunto")
+    print("6. Sair\n")
