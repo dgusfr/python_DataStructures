3d8f670cc78c04f8f58aea039c499541c82b56f5
dgusfr <diegogustavo.fr@gmail.com>
2025-01-11 20:48:49 -0300

    feat: calcular a soma de uma lista de números

--- teste.py
+++ teste.py
@@ -8,3 +8,4 @@ def dobrar(numero): return numero * 2entrada = input("Digite algo: ")
 lista = [1, 2, 3, 4, 5]
 pares = list(filter(lambda x: x % 2 == 0, lista))
 lista.sort(reverse=True)
+soma = sum(lista)
