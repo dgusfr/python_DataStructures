a97d9e38c1568a2ce5a89aebf3d8d033f40585c0
dgusfr <diegogustavo.fr@gmail.com>
2025-08-01 13:59:57 -0300

    fix refact

---
+++ Lists/concat_lists.py
@@ -0,0 +1,5 @@
+products_stock_one = ["Arroz", "Feijão", "Macarrão"]
+products_stock_two = ["Óleo", "Sal", "Açúcar"]
+
+stock_combined = products_stock_one + products_stock_two
+print(f"Stock Combined: {stock_combined}")
