d1d3a282ccc46f64fd9445bc8bb19a68a58dc157
dgusfr <diegogustavo.fr@gmail.com>
2025-08-03 11:36:55 -0300

    feat(add)

--- Sets/difference_lists.py
+++ Sets/difference_lists.py
@@ -1,2 +1,10 @@
-list_Laura = set(input("Enter the list of Laura:").lower().split())
-list_Ana = set(input("Enter the list of Ana:").lower().split())
+list_laura = set(input("Enter the list of Laura:").lower().split())
+list_ana = set(input("Enter the list of Ana:").lower().split())
+
+itens_in_both = list_laura.intersection(list_ana)
+exclusive_laura = list_laura.difference(list_ana)
+exclusive_ana = list_ana.difference(list_laura)
+
+print(f"Itens in both lists {', '.join(itens_in_both)}")
+print(f"Itens exclusive of Laura {', '.join(exclusive_laura)}")
+print(f"Itens exclusive of Ana {', '.join(exclusive_ana)}")
